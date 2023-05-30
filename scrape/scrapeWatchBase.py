781177d7d3af864ab2ed5ed9336705633969b49a
vladimirven001 <vladimir.venkov@mail.mcgill.ca>
2023-05-30 18:54:52 -0400

    trying to prevent the website from timeouting ip address

--- scrape/scrapeWatchBase.py
+++ scrape/scrapeWatchBase.py
@@ -1,22 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+from time import sleep
 
 HEADERS = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
 database = requests.get("https://watchbase.com/watches", headers=HEADERS)
 soup = BeautifulSoup(database.content, "html.parser")
+
+proxies = [
+    {'http': 'http://181.129.74.58:40667', 'https': 'https://181.129.74.58:40667'},
+    {'http': 'http://105.19.63.217:9812', 'https': 'https://105.19.63.217:9812'},
+    {'http': 'http://187.217.54.84:80', 'https': 'https://187.217.54.84:80'}
+]
 
 brands = [] # List to store brand of the watch (roots of the trees)
 models = [] # List to store models (nodes of the tree)
 modelURLs = [] # List to store the URLs of each model
 referenceURLs = [] # List to store the URLs of each watch reference (leafs of the brand trees)
 
-mainDiv = soup.find("div", id = "brand-container")
-mainHeaders = mainDiv.find_all("h2")
+# Extract brand names
+mainDiv = soup.find('div', id = "brand-container")
+mainHeaders = mainDiv.find_all('h2')
 for brand in mainHeaders:
     brands.append(brand.text.strip())
 
-unorderedLists = mainDiv.find_all("ul")
+# Extract model names and model URLs
+unorderedLists = mainDiv.find_all('ul')
 for ul in unorderedLists:
     aTags = ul.find_all('a')
     for a in aTags:
@@ -25,9 +34,24 @@ for ul in unorderedLists:
             models.append(txt)
             modelURLs.append(a['href'])
 
+# Extract reference URLs
+i=0
+j=1
+proxy = proxies[0]
 for url in modelURLs:
-    modelPage = requests.get(url, headers=HEADERS)
+    i += 1
+    print(i)
+    if i == 12:
+        proxy = proxies[j]
+        j += 1
+        print(j)
+    modelPage = requests.get(url, headers=HEADERS, proxies=proxy)
     soupModel = BeautifulSoup(modelPage.content, "html.parser")
-    div = soup.find("div", class_ = "watch-block-container")
+    divReferences = soupModel.find('div', class_ = 'watch-block-container')
+    aTagsReferences = divReferences.find_all('a')
+    for a in aTagsReferences:
+        referenceURLs.append(a['href'])
+    sleep(0.1)
 
+print(referenceURLs)
 
