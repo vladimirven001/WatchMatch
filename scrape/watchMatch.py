a25f15cd8a710056b332e3eb473071f80f4c4541
vladimirven001 <vladimir.venkov@mail.mcgill.ca>
2023-09-10 23:00:16 -0400

    working ish watchMatch

--- scrape/watchMatch.py
+++ scrape/watchMatch.py
@@ -11,29 +11,30 @@ except:
                                     "dialMaterial","dialIndexes","dialHands"])
     
 # def watch_match(givenWatch):
-    # Combine descriptors into a single column
-    df['CombinedDescriptors'] = df.apply(lambda row: [str(row[col]) for col in df.columns], axis=1)
+#     # Combine descriptors into a single column
+#     df['CombinedDescriptors'] = df.apply(lambda row: [str(row[col]) for col in df.columns], axis=1)
 
-    # Convert descriptors to binary indicators
-    mlb = MultiLabelBinarizer()
-    descriptor_matrix = mlb.fit_transform(df['CombinedDescriptors'])
-    print(descriptor_matrix)
+#     # Convert descriptors to binary indicators
+#     mlb = MultiLabelBinarizer()
+#     descriptor_matrix = mlb.fit_transform(df['CombinedDescriptors'])
+#     print(descriptor_matrix)
 
-    # Convert the given watch descriptors to binary
-    given_watch_binary = mlb.transform([list(givenWatch)])
-    print(given_watch_binary)
+#     # Convert the given watch descriptors to binary
+#     given_watch_binary = mlb.transform([list(givenWatch)])
+#     print(given_watch_binary)
 
-    # Calculate Jaccard similarity between the given watch and each row
-    similarities = jaccard_score(given_watch_binary, descriptor_matrix, average=None)
+#     # Calculate Jaccard similarity between the given watch and each row
+#     similarities = jaccard_score(given_watch_binary, descriptor_matrix, average=None)
 
-    # Find the index of the row with the highest similarity
-    best_match_index = similarities.argmax()
+#     # Find the index of the row with the highest similarity
+#     best_match_index = similarities.argmax()
 
-    # Get the best matching row
-    best_match_row = df.loc[best_match_index]
+#     # Get the best matching row
+#     best_match_row = df.loc[best_match_index]
 
-    print("Best Matching Row:")
-    print(best_match_row)
+#     print("Best Matching Row:")
+#     print(best_match_row)
+
 
 def watch_match(givenWatch):
     # Combine descriptors into a single column
@@ -43,7 +44,7 @@ def watch_match(givenWatch):
     mlb = MultiLabelBinarizer()
     descriptor_matrix = mlb.fit_transform(df['CombinedDescriptors'])
     given_watch_binary = mlb.transform([list(givenWatch)])  
-X   
+   
     # Define a custom Jaccard similarity function
     def jaccard_similarity(a, b):
         intersection = sum(min(x, y) for x, y in zip(a, b))
